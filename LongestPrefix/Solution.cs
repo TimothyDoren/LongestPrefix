bb6358841c86fa04bbc795f16b29731ec7e6c788
Tim D <124634979+TimothyDoren@users.noreply.github.com>
2023-05-29 21:11:16 -0400

    Add project files.

---
+++ LongestPrefix/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LongestPrefix
+{
+
+public class Solution
+{
+    public string LongestCommonPrefix(string[] strs)
+    {
+        if (strs.Length == 0) return "";
+        string prefix = strs[0];
+        for (int i = 1; i < strs.Length; i++)
+        {
+            while (strs[i].IndexOf(prefix) != 0)
+            {
+                prefix = prefix.Substring(0, prefix.Length - 1);
+            }
+        }
+        return prefix;
+    }
+}
+
+}
